b319339fe25940c9ff87971fca9fdd1e1fc37f3a
vine90et <vineetsurve59@gmail.com>
2026-01-23 17:52:14 +0530

    ready for production

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,11 +1,22 @@
 // const express = require("express");
 import express from "express";
 import dotenv from "dotenv"
+import path from "path";
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=> console.log('✅ app is running on port:  '+PORT));
+//make Production ready
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (_,res)=>{
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    })
+}
+
+app.listen(PORT, ()=> console.log('✅ app is running on port: '+PORT));
