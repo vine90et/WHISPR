3886178595243dee7f1034abbaae654b265e419a
vine90et <vineetsurve59@gmail.com>
2026-01-23 17:05:22 +0530

    Initial commit

---
+++ Backend/src/server.js
@@ -0,0 +1,11 @@
+// const express = require("express");
+import express from "express";
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=> console.log('✅ app is running on port:  '+PORT));
